16f43b5eadf71c3aaa83ce1fe034f8dcd956fb49
Neel <neelakantanaiduchittela@gmail.com>
2025-03-26 18:46:45 +0530

    Made a blog page with reactjs

---
+++ src/Nav.js
@@ -0,0 +1,51 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { useEffect } from "react";
+import { useStoreState, useStoreActions } from "easy-peasy";
+
+const Nav = () => {
+  const posts = useStoreState((state) => state.posts);
+  const search = useStoreState((state) => state.search);
+  const setSearch = useStoreActions((actions) => actions.setSearch);
+  const setSearchResults = useStoreActions(
+    (actions) => actions.setSearchResults
+  );
+
+  useEffect(() => {
+    const filterredResults = posts.filter(
+      (post) =>
+        post.body.toLowerCase().includes(search.toLowerCase()) ||
+        post.title.toLowerCase().includes(search.toLowerCase())
+    );
+
+    setSearchResults(filterredResults.reverse());
+  }, [posts, search]);
+
+  return (
+    <nav className="Nav">
+      <form className="searchForm" onSubmit={(e) => e.preventDefault()}>
+        <label htmlFor="search">Search Posts</label>
+        <input
+          id="search"
+          placeholder="Search Posts"
+          type="text"
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+        />
+      </form>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/post">Post</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Nav;
